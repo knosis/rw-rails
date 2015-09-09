476d4e030a05b7ccef10b83f13f0df5db29f0075
Tarik B <tarik.bennett@gmail.com>
2015-09-09 11:33:17 -0700

    Initialize rails. Setup user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def update
+    @login = params[:login]
+    @user = User.find_by(:login => @login)
+  end
+
+  def edit
+    @login = params[:login]
+    @user = User.find_by(:login => @login)
+  end
+
+  def destroy
+    @login = params[:login]
+    @user = User.find_by(:login => @login)
+  end
+
+  def index
+  end
+
+  def show
+    @login = params[:login]
+    @user = User.find_by(:login => @login)
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+end
